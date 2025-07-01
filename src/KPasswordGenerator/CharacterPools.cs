0c4cb4633e452a0eff0b7cca3fa55cdb00ef5e31
Kirill Polishchuk <kirpolis@gmail.com>
2025-07-02 11:51:08 +1200

    Added PasswordSettings.WithDefaults (#7)
    * Added new methods
    
    * Added default methods

--- src/KPasswordGenerator/CharacterPools.cs
+++ src/KPasswordGenerator/CharacterPools.cs
@@ -6,5 +6,7 @@ public static class CharacterPools
 
     public const string UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+    public const string Digits = "0123456789";
+
     public const string Special = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 }
--- src/KPasswordGenerator/PasswordSettings.cs
+++ src/KPasswordGenerator/PasswordSettings.cs
@@ -16,4 +16,23 @@ public sealed class PasswordSettings
     }
 
     public ICollection<CharacterRequirement> CharacterRequirements { get; }
+
+    public static PasswordSettings WithDefaults(int minLower = 1, int minUpper = 1, int minDigits = 1, int minSpecial = 1)
+    {
+        List<CharacterRequirement> requirements = [];
+
+        if (minLower > 0)
+            requirements.Add(new CharacterRequirement(minLower, CharacterPools.LowerCase));
+
+        if (minUpper > 0)
+            requirements.Add(new CharacterRequirement(minUpper, CharacterPools.UpperCase));
+
+        if (minDigits > 0)
+            requirements.Add(new CharacterRequirement(minDigits, CharacterPools.Digits));
+
+        if (minSpecial > 0)
+            requirements.Add(new CharacterRequirement(minSpecial, CharacterPools.Special));
+
+        return new PasswordSettings(requirements);
+    }
 }
